daaf2a49033385c84decd9063fedd3715309263a
Irahmawati234 <rw169644@gmail.com>
2025-07-27 20:34:03 +0700

    uas

--- app/src/main/java/com/meli/inventory/network/ApiClient.kt
+++ app/src/main/java/com/meli/inventory/network/ApiClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://10.0.2.2/api_syifa/"
+    private const val BASE_URL = "http://10.0.2.2/api_salwa/"
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
